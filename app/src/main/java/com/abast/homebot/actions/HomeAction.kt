58a06247530969a497a6c9c4dc7c508f7bfda277
Lorenzo Testa <kklorenzotesta@outlook.it>
2019-03-19 21:28:57 +0100

    fix wrong animation

--- app/src/main/java/com/abast/homebot/actions/HomeAction.kt
+++ app/src/main/java/com/abast/homebot/actions/HomeAction.kt
@@ -177,7 +177,7 @@ data class LaunchApp(val uri: String) : HomeAction() {
             }
             activity.finish()
             activity.startActivity(shortcut)
-            activity.overridePendingTransition(R.anim.abc_slide_in_top, R.anim.abc_slide_out_top)
+            activity.overridePendingTransition(R.anim.abc_slide_in_bottom, R.anim.abc_slide_out_top)
         } catch (e: URISyntaxException) {
             e.printStackTrace()
             launchMainActivity(context)
